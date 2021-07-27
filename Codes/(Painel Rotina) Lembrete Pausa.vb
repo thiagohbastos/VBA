6ec8f5798bb0478918f539a07277eb52f82d7170
ThiagohBastos <thiagoh_bastos@hotmail.com>
2021-07-27 09:44:37 -0300

    Update (Painel Rotina) Lembrete Pausa.vb

--- Codes/(Painel Rotina) Lembrete Pausa.vb
+++ Codes/(Painel Rotina) Lembrete Pausa.vb
@@ -1,1 +1,21 @@
-' Lembrete de pausa de 1 em 1 h.
+'AUTOR: THIAGO BASTOS
+'GITHUB: THIAGOHBASTOS
+
+'MACRO RESPONSÁVEL POR GERAR AVISO EM TELA DE HORA EM HORA
+Option Explicit
+Dim aviso_cronometro As Date
+
+Sub Executa_por_tempo()
+
+aviso_cronometro = Now + TimeValue("01:00:00")
+
+Call Application.OnTime(aviso_cronometro, "executa_por_tempo")
+MsgBox ("Pausa Para Água!")
+
+End Sub
+
+Sub finaliza_por_tempo()
+
+Call Application.OnTime(aviso_cronometro, "executa_por_tempo", , False)
+
+End Sub
